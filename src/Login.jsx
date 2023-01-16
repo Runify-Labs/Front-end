e132c98c36f15a7c8ba250398a0e2be7cb87d73a
nathan7432 <steelersfan5052@yahoo.com>
2023-01-15 20:49:18 -0500

    fixed image bug

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import running from './assets/stickman-running.gif'
-// import spotifyLogo from './assets/Spotify_Logo_RGB_Green2.png'
+import running from './assets/stickman-running4.gif'
+import spotifyLogo from './assets/Spotify_Logo_RGB_Green2.png'
 
 const Login = () => {
 
@@ -12,7 +12,7 @@ const Login = () => {
         <span className="text-6xl font-bold mb-4 text-[#321325]">Runify</span>
         <div className="flex flex-row items-center">
           <span className="text-[#321325] font-thin mr-2">In partnership with</span>
-          {/* <img src={spotifyLogo} /> */}
+          <img src={spotifyLogo} />
         </div>
         <img className="w-1/12" src={running} />
         <a className="bg-[#1DB954] py-2 px-10 m-2 rounded-3xl" href={AUTH_URL}>
--- src/Result.jsx
+++ src/Result.jsx
@@ -1,12 +1,17 @@
 import React, { useEffect } from "react";
+import musicNote from "./assets/music-note.png"
 
 const Result = ({ item, setSelection, selection }) => {
 
   var comp;
+  var images = item.album.images;
+  if(images.length === 0)
+    item.album.images[0] = musicNote
+  console.log(images)
 
   if(item.type === 'track') {
     comp = (<div onClick={() => setSelection([...selection, item])}>
-      <img src={item.album.images[2].url}/>
+      <img src={images[images.length-1].url}/>
       <div>{item.name}</div>
       <div>{item.artists[0].name}</div>
     </div>)
